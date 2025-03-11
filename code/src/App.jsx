7a6e2fdfe4883ab273b574a7ec8cea748d312be6
j3 <jeki.ang3l@gmail.com>
2025-03-12 00:11:09 +0200

    admin classes wireframe

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react'
 import './css/Global.css'
 import DishMenu from './components/DishMenu'
+import AdminClasses from './components/AdminClasses'
 
 export default function App() {
 
   /*
    CURRENTLY CODE IS FOR WIREFRAME AND UI DESIGN
    NO REAL LOGIC FOR NOW
+
+Wireframes done:
+           <DishMenu />
+        <AdminClasses />
+
+
   */
- 
+
   return (
     <div>
       <div className='TopHeader'>
-        <h1 className="title">תפריט המזנון של אונו</h1>
+        <h1 className="title">תפריט המזנון של אונו - ניהול כיתות</h1>
         <div className="HeaderCTRL">
-          <button>d</button><button>s</button><button>s</button>
+          <button>לצ׳ט עם מייקל</button><button>שמור</button><button>יציאה</button>
         </div>
-        </div>
+      </div>
       <div >
-        <DishMenu />
+        <AdminClasses />
       </div>
     </div >
   )
--- code/src/components/AdminClasses.jsx
+++ code/src/components/AdminClasses.jsx
@@ -3,7 +3,76 @@ import React from 'react'
 export default function AdminClasses() {
   return (
     <div>
-      
+        <table >
+          <thead>
+            <tr><th>מספר חדר</th><th>מיקום</th><th>מותר למשלוח</th></tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>101</td>
+              <td>צפון</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+            <tr>
+              <td>102</td>
+              <td>דרום</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+            <tr>
+              <td>104</td>
+              <td>ספריה</td>
+              <td><input type="checkbox" value="0"></input></td>
+            </tr>
+            <tr>
+              <td>201</td>
+              <td>צפון</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+            <tr>
+              <td>202</td>
+              <td>צפון</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+            <tr>
+              <td>203</td>
+              <td>מעבדת סייבר</td>
+              <td><input type="checkbox" value="0"></input></td>
+            </tr>
+            <tr>
+              <td>204</td>
+              <td>מעבדת מחקר</td>
+              <td><input type="checkbox" value="0"></input></td>
+            </tr>
+            <tr>
+              <td>300</td>
+              <td>צפון</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+            <tr>
+              <td>310</td>
+              <td>דרום</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+            <tr>
+              <td>320</td>
+              <td>גג</td>
+              <td><input type="checkbox" checked="true"></input></td>
+            </tr>
+          </tbody>
+        </table>
+
+
+
+
+
     </div>
   )
 }
+
+
+
+/*
+
+
+
+*/
